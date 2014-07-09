36b85e7bdd3635df2dbf6edc36f70622a2dd5ba8
ruslankus <ruslankus@yahoo.com>
2014-07-09 19:25:27 +0300

    мелкий фикс

--- protected/helpers/Label.php
+++ protected/helpers/Label.php
@@ -5,15 +5,14 @@ class Label
     public static function Get($language=null)
     {
         
-
         $labels = array();    
-        
-       
+               
         $connection = Yii::app()->labels;
         $sql="SELECT label, value FROM labels";
         $dataReader=$connection->createCommand($sql)->query();
+        // привязываем первое поле (label) к переменной $label
         $dataReader->bindColumn(1,$label);
-        // привязываем второе поле (email) к переменной $email
+        // привязываем второе поле (value) к переменной $value
         $dataReader->bindColumn(2,$value);
         
         while($dataReader->read()!==false)
